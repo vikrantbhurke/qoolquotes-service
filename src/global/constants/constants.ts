fb27f6c2cf4b3a84f2514448f488119c3d4b6552
Vikrant Bhurke <bhurkevi@gmail.com>
2024-12-03 19:56:36 +0530

    minor update

--- src/global/constants/constants.ts
+++ src/global/constants/constants.ts
@@ -1,4 +1,4 @@
 export const PAGE_SIZE = 30;
-export const RANDOM_PAGE_SIZE = 10;
+export const RANDOM_PAGE_SIZE = 20;
 export const PLAYLIST_LIMIT = 5;
 export const QUOTE_LIMIT = 5;
--- src/playlist/playlist.error.ts
+++ src/playlist/playlist.error.ts
@@ -1,5 +1,5 @@
 export enum PlaylistError {
-  Name = "Must be 3 - 30 characters. Must not have digits, and symbols.",
+  Name = "Must be 3 - 20 characters. Must not have digits, and symbols.",
   Description = "Must be 100 characters max.",
   Access = "Must be Public, Private or Protected.",
 }
