60f66da353ac218cf4d6e15a9ea30ca7193cb868
Vikrant Bhurke <bhurkevi@gmail.com>
2024-12-16 19:52:05 +0530

    increase char limit for cited quote & message

--- src/cited-quote/cited-quote.error.ts
+++ src/cited-quote/cited-quote.error.ts
@@ -1,5 +1,5 @@
 export enum CitedQuoteError {
   Content = "Must be between 5 and 500 characters.",
-  Author = "Must be between 5 and 30 characters.",
+  Author = "Must be between 5 and 50 characters.",
   Email = "Must be in valid email format.",
 }
