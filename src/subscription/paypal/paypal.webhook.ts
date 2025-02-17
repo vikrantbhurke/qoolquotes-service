fde9ba126654b1d04bd5ac84378bdac33d971883
Vikrant Bhurke <bhurkevi@gmail.com>
2025-02-17 20:15:38 +0530

    return 200 from paypal webhook method

--- src/subscription/paypal/paypal.webhook.ts
+++ src/subscription/paypal/paypal.webhook.ts
@@ -5,6 +5,7 @@ export class PayPalWebhook {
     try {
       let event = request.body;
       console.log(event);
+      return response.status(200).json({ message: "Webhook received." });
     } catch (error: any) {
       return response.status(500).json({ message: error.message });
     }
