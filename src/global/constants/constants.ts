039482493a07847dfd446ee24c51dabd3abcceec
Vikrant Bhurke <bhurkevi@gmail.com>
2025-02-26 01:59:03 +0530

    temporarily change playlist, quote constants for testing

--- src/global/constants/constants.ts
+++ src/global/constants/constants.ts
@@ -1,7 +1,11 @@
 export const PAGE_SIZE = 60;
 export const CREATE_PLAYLIST_FREE_LIMIT = 1;
-export const CREATE_PLAYLIST_PAID_LIMIT = 10;
+// export const CREATE_PLAYLIST_PAID_LIMIT = 10;
+export const CREATE_PLAYLIST_PAID_LIMIT = 3;
 export const SAVE_PLAYLIST_FREE_LIMIT = 2;
-export const SAVE_PLAYLIST_PAID_LIMIT = 20;
-export const QUOTE_FREE_LIMIT = 500;
-export const QUOTE_PAID_LIMIT = 10000;
+// export const SAVE_PLAYLIST_PAID_LIMIT = 20;
+export const SAVE_PLAYLIST_PAID_LIMIT = 3;
+// export const QUOTE_FREE_LIMIT = 500;
+// export const QUOTE_PAID_LIMIT = 10000;
+export const QUOTE_FREE_LIMIT = 5;
+export const QUOTE_PAID_LIMIT = 10;
