8cef6701775e2be77fe0b68b7837f070544b752c
Vikrant Bhurke <bhurkevi@gmail.com>
2025-02-26 11:51:47 +0530

    change user password error message

--- src/user/user.error.ts
+++ src/user/user.error.ts
@@ -3,6 +3,6 @@ export enum UserError {
   Lastname = "Must be 2 - 20 characters. Must not have digits and symbols.",
   Username = "Must be 3 - 20 characters. Must not have spaces.",
   Email = "Must be in valid email format.",
-  Password = "Must be 6 - 20 characters. Must have an uppercase & a lowercase letter, a number, and a symbol. Must not have spaces.",
+  Password = "Must be 8 - 20 characters. Must have an uppercase & a lowercase letter, a number, and a symbol. Must not have spaces.",
   Gender = "Must be valid gender. It should be either Male, Female or Other.",
 }
